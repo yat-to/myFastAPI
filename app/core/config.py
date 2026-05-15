bea72296ea9155872030eb175bb1f16c43ec2374
yat-to <mhidayatdarmawan@gmail.com>
2026-05-15 15:46:40 +0800

    adding access token

--- app/core/config.py
+++ app/core/config.py
@@ -7,5 +7,6 @@ class Settings:
     PROJECT_NAME: str = "My FastAPI App"
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     DATABASE_URL: str = os.getenv("DATABASE_URL")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1440))
 
 settings = Settings()
--- app/core/security.py
+++ app/core/security.py
@@ -1,14 +1,15 @@
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
+from jose import JWTError, jwt, ExpiredSignatureError
 from passlib.context import CryptContext
 import os
 from dotenv import load_dotenv
+from app.core.config import settings
+from fastapi import HTTPException
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "ganti-secret-key-yang-aman")
+SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -20,12 +21,14 @@ def verify_password(plain: str, hashed: str) -> bool:
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 def decode_token(token: str) -> dict:
     try:
         return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Session expired, silakan login kembali.")
     except JWTError:
-        return None
+        raise HTTPException(status_code=401, detail="Token tidak valid atau rusak.")
